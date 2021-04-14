e3202eea82a57bab33414dd415bead2448c9bde5
Malcolm Knapp <malcolm@mfhkconsulting.com>
2021-04-14 04:25:51 +0000

    initial commit

---
+++ Arduino/ServoControlI2C/ServoControlI2C.ino
@@ -0,0 +1,83 @@
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <ros.h>
+
+#include <std_msgs/UInt16.h>
+
+ros::NodeHandle  nh;
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define BASE_SERVO 0
+#define ELBOW_SERVO 1
+#define PEN_SERVO 2
+
+int pen_goal = 300;
+int elbow_goal = 190;
+int base_goal = 190;
+int pen_current_pos = 300;
+int elbow_current_pos = 190;
+int base_current_pos = 190;
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+void base_servo_cb( const std_msgs::UInt16&  cmd_msg){
+  base_goal = cmd_msg.data;  
+  nh.logdebug(cmd_msg.data);
+ 
+}
+
+void elbow_servo_cb( const std_msgs::UInt16&  cmd_msg){
+  elbow_goal = cmd_msg.data; 
+  nh.logdebug(cmd_msg.data);
+
+}
+
+void pen_cb( const std_msgs::UInt16&  cmd_msg){
+  pwm.setPWM(PEN_SERVO, 0, cmd_msg.data); //set servo pulse width, should be 160 or 300
+  nh.logdebug(cmd_msg.data);
+
+}
+ros::Subscriber<std_msgs::UInt16> sub1("base", base_servo_cb);
+ros::Subscriber<std_msgs::UInt16> sub2("elbow", elbow_servo_cb);
+ros::Subscriber<std_msgs::UInt16> sub3("pen", pen_cb);
+
+void setup(){
+  pinMode(13, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub1);
+  nh.subscribe(sub2);
+  nh.subscribe(sub3);
+  pwm.begin();
+  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  Wire.setClock(400000);
+  delay(10);
+
+}
+
+void loop(){
+  if (abs(base_current_pos - base_goal) > 10) {
+    if (base_goal > base_current_pos) {base_current_pos += 3;}
+    if (base_goal < base_current_pos) {base_current_pos -= 3;}
+    pwm.setPWM(BASE_SERVO, 0, base_current_pos);
+  }
+
+    if (abs(elbow_current_pos - elbow_goal) > 10) {
+    if (elbow_goal > elbow_current_pos) {elbow_current_pos += 3;}
+    if (elbow_goal < elbow_current_pos) {elbow_current_pos -= 3;}
+    pwm.setPWM(ELBOW_SERVO, 0, elbow_current_pos);
+  }
+
+  nh.spinOnce();
+  delay(20);
+}
